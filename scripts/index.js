07b0f7e77d8e73d886d181d63291764db13e20fa
Piyal Shah <piyalsha007@gmail.com>
2025-03-14 13:33:30 +0600

    dynamically get nav category name and place them in nav item

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,27 @@
 
 function loadCategories() {
+    // 1 - fetch the data
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        // 2 - convert the promise to json
         .then(response => response.json())
-    .then(data => console.log((data)))
+        // 3 - sent data to display
+        .then(data => displayCategories((data.categories)))
+}
+
+function displayCategories(categories) {
+    // get the container
+    const cardNav = document.getElementById('card-nav')
+    //loop operation on array of object
+    for (const cat of categories) {
+        //create element
+        const navDiv = document.createElement('div')
+        navDiv.innerHTML = `
+        <button class="btn btn-sm">${cat.category}</button>
+        `
+        //append the element
+        cardNav.appendChild(navDiv)
+    }
+    
 }
 
 loadCategories()
